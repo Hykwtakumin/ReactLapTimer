23fe0d88aa8ac48ea3fa867dee48114e220d648a
Takumi Hayakawa <Hykwtakumin@users.noreply.github.com>
2018-10-18 22:02:34 +0900

    ドキュメントを整備
    コード内コメントとREADMEを整備

--- src/components/LapTimer.tsx
+++ src/components/LapTimer.tsx
@@ -34,7 +34,7 @@ interface defaultProps {
 interface defaultState {
   isStarted: boolean; //タイマーが作動しているか否か
   seconds: number; //カウントしている
-  laps: Array<number>;
+  laps: Array<number>; //ラップタイムが格納される配列
 }
 
 class LapTimer extends React.Component<defaultProps, defaultState> {
@@ -59,10 +59,10 @@ class LapTimer extends React.Component<defaultProps, defaultState> {
     return `${m}:${s}`;
   };
 
-  //ラップタイムをDLできるようにする
+  //ラップタイムをJSON形式でDLできるようにする
   handleSaveButton = () => {
     this.timerStop();
-    const time = this.state.seconds; //最終的なタイム
+    const time = this.state.seconds;
     const laps = this.state.laps.map((lap, index) => {
       return `Lap${index + 1}:${this.formatTime(lap)} +${this.formatTime(
         time - lap
@@ -79,10 +79,11 @@ class LapTimer extends React.Component<defaultProps, defaultState> {
       encodeURIComponent(JSON.stringify(results));
   };
 
+  //ラップタイムを追加
   handleLapButton = () => {
-    const lapSeconds = this.state.seconds; //Lapボタンを押した時点での秒数
+    const lapSeconds = this.state.seconds; //Lapボタンを押した時点での秒数を
     const laps = this.state.laps;
-    laps.push(lapSeconds);
+    laps.push(lapSeconds); //配列に格納していく(後でリスト状に表示する)
     this.setState({ laps: laps });
   };
 
@@ -101,11 +102,15 @@ class LapTimer extends React.Component<defaultProps, defaultState> {
     }
   };
 
+  //タイマー処理
+  //secondsに1足す処理をtimerStart()で毎秒呼ぶ
   timer = () => {
     const currentSeconds = this.state.seconds as number;
     this.setState({ seconds: currentSeconds + 1 });
   };
 
+  //JSのsetIntervalｊはさほど正確ではないので注意
+  //厳密に測定する場合はwindow.perfomance.now()を使うと良い
   timerStart = () => {
     if (!this.state.isStarted) {
       this.setState({ isStarted: true });
@@ -113,6 +118,7 @@ class LapTimer extends React.Component<defaultProps, defaultState> {
     }
   };
 
+  //Timerを停止させる
   timerStop = () => {
     if (this.state.isStarted) {
       clearInterval(this.counter);
@@ -120,6 +126,7 @@ class LapTimer extends React.Component<defaultProps, defaultState> {
     }
   };
 
+  //再生と停止とでアイコンを分けて返す
   handleIcon = (): string => {
     if (this.state.isStarted) {
       return "pause";
@@ -128,23 +135,28 @@ class LapTimer extends React.Component<defaultProps, defaultState> {
     }
   };
 
+  //componentがDOMから削除される時タイマーも削除する
   componentWillUnmount() {
     clearInterval(this.counter);
   }
 
+  //描画していく
+  //JSXではJSの中にHTMLを書ける
   render() {
     return (
       <div className={css(styles.container)}>
         <Layout>
           <Content>
+            {/* フォーマットした時間を表示する部分 */}
             <h1 className={css(styles.timerArea)}>
               {this.formatTime(this.state.seconds)}秒
             </h1>
             <div className={css(styles.controlerArea)}>
+              {/* リセットボタン */}
               <Button onClick={this.handleResetButton} icon="redo" size="large">
                 Reset
               </Button>
-
+              {/* 測定開始/停止ボタン */}
               <Button
                 onClick={this.handleTimerButton}
                 icon={this.handleIcon()}
@@ -152,12 +164,13 @@ class LapTimer extends React.Component<defaultProps, defaultState> {
               >
                 {!this.state.isStarted ? "Start" : "Stop"}
               </Button>
-
+              {/* ラップタイム追加ボタン */}
               <Button onClick={this.handleLapButton} icon="plus" size="large">
                 Lap
               </Button>
             </div>
           </Content>
+          {/* 配列に格納したラップタイムをリスト状に表示 */}
           <Content className={css(styles.lapListArea)}>
             <List
               header={<div>Laps</div>}
@@ -165,6 +178,7 @@ class LapTimer extends React.Component<defaultProps, defaultState> {
               dataSource={this.state.laps}
               renderItem={item => (
                 <List.Item>
+                  {/* ラップタイム追加ボタンを押した段階での秒数 */}
                   Lap{this.state.laps.indexOf(item) + 1} :
                   {this.formatTime(item)}
                   {`+${this.formatTime(this.state.seconds - item)}秒`}
@@ -173,6 +187,7 @@ class LapTimer extends React.Component<defaultProps, defaultState> {
             />
           </Content>
           <Footer>
+            {/* ラップタイムダウンロードボタン */}
             <Button onClick={this.handleSaveButton} icon="download">
               タイムを保存する
             </Button>
